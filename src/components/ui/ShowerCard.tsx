9b57827dba6dce6100caf30ee99ae3298ba5858b
Devanshi Brower <devanshibrower@gmail.com>
2026-01-29 21:50:07 -0800

    Cleaner layout of card with image spanning full width, better spacing between content and cleaner hierarchy using colors.

--- src/components/ui/ShowerCard.tsx
+++ src/components/ui/ShowerCard.tsx
@@ -4,33 +4,57 @@ export function ShowerCard({ shower }: { shower: MeteorShower }) {
   return (
     <div
       className="
-    border-gray-200
-    border-stroke
-    rounded-lg
-    p-8
-    text-white-200
-    "
+        border-gray-300
+        border-stroke
+        rounded-lg
+        p-8
+        text-foreground
+        max-w-2xl
+        flex
+        flex-col
+        items-start
+        justify-start
+        "
     >
       <img
         src={shower.image}
         alt={shower.name}
         className="
-        w-[100px]
-        h-[100px]
-        mb-4"
+            w-full
+            h-[300px]
+            object-cover
+            mb-4"
       />
-      <h3
+      <div
         className="
-        text-lg 
-        font-bold 
-        mb-2"
+            flex flex-col
+            items-start
+            gap-2"
       >
-        {shower.name}
-      </h3>
-      <p>{shower.timing}</p>
-      <p>{shower.description}</p>
-      <p>{shower.velocity}</p>
-      <p>{shower.nextPeak}</p>
+        <h3
+          className="
+            text-lg 
+            font-bold 
+            mb-2"
+        >
+          {shower.name}
+        </h3>
+        <p className="text-sm text-gray-400">{shower.timing}</p>
+        <p>{shower.description}</p>
+        <p className="gap-1 text-gray-400">
+          <span className="font-bold">Velocity: </span>
+          <span>{shower.velocity}</span>
+        </p>
+        <p className="gap-1 text-gray-400">
+          <span
+            className="
+            font-bold"
+          >
+            Next Peak:{" "}
+          </span>
+          <span>{shower.nextPeak}</span>
+        </p>
+      </div>
     </div>
   );
 }
