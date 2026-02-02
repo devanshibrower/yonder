aac33cfe028695bfc92bf0525212e2e0c661b8ce
Devanshi Brower <devanshibrower@gmail.com>
2026-02-02 13:37:28 -0800

    Created JSON for meteor showers in 2026 based on information from International Meteor Organization

---
+++ src/data/types.ts
@@ -0,0 +1,36 @@
+export type VelocityCategory = "swift" | "medium" | "slow";
+export type ParentObjectType = "asteroid" | "comet";
+
+export type MeteorShower = {
+  id: string;
+  code: string;
+  name: string;
+  activePeriod: {
+    start: string;
+    end: string;
+  };
+  peak: {
+    month: number;
+    dayOfMonth: number;
+  };
+  zhr: number;
+  velocity: {
+    kmPerSec: number;
+    category: VelocityCategory;
+  };
+  radiant: {
+    ra: string;
+    dec: string;
+    constellation: string;
+  };
+  parentObject: {
+    name: string;
+    type: ParentObjectType;
+  };
+  moonPhase2026: {
+    percentIlluminated: number;
+    phaseName: string;
+  };
+  description: string;
+  image: string;
+};
